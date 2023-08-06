1605d96f7f101169ac03c502492e5f9c499778d5
BhaskarJagi <bhaskarjagi.gmail.com>
2023-08-06 21:47:40 +0530

    submitting code-Sun Aug 06 2023 21:47:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from './Tabs.js'
+
 
 const App = () => {
+  
+  const tabs = [
+    {
+      title: "Apple",
+      content: "An Apple a day keep the doctor away."
+    },
+    {
+      title: "Orange",
+      content: "Oranges are citrus fruits."
+    },
+    {
+      title: "Mango",
+      content: "Mango is a seasonal friut of summer."
+    }
+  ]
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tabs tabs={tabs}/>
     </div>
   )
 }
